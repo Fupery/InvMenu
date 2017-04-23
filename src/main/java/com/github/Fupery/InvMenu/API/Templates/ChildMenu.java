39400eabc756dc4061c3c65cca0650443255e9a0
Fupery <Fupery1@gmail.com>
2017-04-23 20:04:38 +1000

    Add documentation for childmenu

--- src/main/java/com/github/Fupery/InvMenu/API/Templates/ChildMenu.java
+++ src/main/java/com/github/Fupery/InvMenu/API/Templates/ChildMenu.java
@@ -1,8 +1,13 @@
 package com.github.Fupery.InvMenu.API.Templates;
 
+import com.github.Fupery.InvMenu.API.Event.MenuCloseReason;
 import com.github.Fupery.InvMenu.API.Handler.CacheableMenu;
 import org.bukkit.entity.Player;
 
+/**
+ * Implement this interface to give your menu child menu behaviour:
+ * if it is closed with {@link MenuCloseReason#BACK}, its parent will automatically be opened.
+ */
 public interface ChildMenu extends MenuTemplate {
     CacheableMenu getParent(Player viewer);
 }
